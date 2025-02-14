2b6003edb2f52053aa22edcef74b93c612cb6c7e
chrehall68 <chrehall68@gmail.com>
2025-02-13 20:34:20 -0800

    upgrade minimization algorithm to O(N**2)

--- src/format/macroFormat.cpp
+++ src/format/macroFormat.cpp
@@ -71,7 +71,7 @@ pair<vector<TokenInfo>, SourceLocation> getTokens(SourceManager &sm)
     return {tokens, tok.getLocation()};
 }
 
-pair<int, vector<int>> mostValuableSubarray(vector<int> &tokens, map<int, int> &weights, int k)
+pair<int, vector<int>> mostValuableSubarrayV1(vector<int> &tokens, map<int, int> &weights, int k)
 {
     // brute force O(N**3) solution
     int maxWorth = numeric_limits<int>::min();
@@ -100,7 +100,7 @@ pair<int, vector<int>> mostValuableSubarray(vector<int> &tokens, map<int, int> &
             }
 
             // now scan the remainder of the string to get counts
-            int counts = 0;
+            int counts = 1;
             int length = 0;
             for (int l = j + 1; l < tokens.size(); ++l)
             {
@@ -138,6 +138,208 @@ pair<int, vector<int>> mostValuableSubarray(vector<int> &tokens, map<int, int> &
         result.push_back(tokens[i]);
     }
     return {maxWorth, result};
+}
+vector<int> sortCyclicShifts(const vector<int> &arr)
+{
+    int n = arr.size();
+
+    // p holds permuation (order)
+    // c holds equivalence class
+    vector<int> p(n), c(n);
+
+    // sort by first letter
+    // then use that knowledge to combine 2 strings of length l
+    // to make a string of length l*2
+
+    // sort by first letter
+    for (int i = 0; i < n; ++i)
+    {
+        p[i] = i;
+    }
+    std::sort(p.begin(), p.end(), [&arr](int a, int b)
+              { return arr[a] < arr[b]; });
+
+    // now fill in equivalency classes
+    c[p[0]] = 0;
+    int classes = 1;
+    for (int i = 1; i < n; ++i)
+    {
+        if (arr[p[i]] != arr[p[i - 1]])
+        {
+            // new equivalency class
+            ++classes;
+        }
+        c[p[i]] = classes - 1;
+    }
+
+    // now we can combine strings
+    vector<int> pn(n);
+    vector<int> cn(n);
+    vector<int> counts(n); // preallocate space for the counts array used for the count sort
+    for (int k = 0; (1 << k) < n; ++k)
+    {
+        // first, populate p_n
+        for (int i = 0; i < n; ++i)
+        {
+            pn[i] = p[i] - (1 << k);
+            if (pn[i] < 0)
+            {
+                pn[i] += n;
+            }
+        }
+
+        // then sort by the item
+        // but first, clear (only the items that we will use in) counts
+        fill(counts.begin(), counts.begin() + classes, 0);
+        for (int i = 0; i < n; ++i)
+        {
+            counts[c[pn[i]]]++;
+        }
+        // accumulate for counting sort
+        for (int i = 1; i < classes; ++i)
+        {
+            counts[i] += counts[i - 1];
+        }
+        // finish count sort
+        for (int i = n - 1; i > -1; --i)
+        {
+            p[--counts[c[pn[i]]]] = pn[i];
+        }
+
+        cn[p[0]] = 0;
+        classes = 1;
+        for (int i = 1; i < n; ++i)
+        {
+            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << k)) % n]};
+            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << k)) % n]};
+            if (cur != prev)
+            {
+                ++classes;
+            }
+            cn[p[i]] = classes - 1;
+        }
+        // swap c and cn
+        c.swap(cn);
+    }
+    return p;
+}
+vector<int> constructSuffixArray(vector<int> &arr)
+{
+    arr.push_back(-1);
+    vector<int> sortedShifts = sortCyclicShifts(arr);
+    arr.pop_back();
+    sortedShifts.erase(sortedShifts.begin()); // everything except front; O(n) operation
+    return sortedShifts;
+}
+vector<int> constructLCPArray(vector<int> &arr, vector<int> &suffixArray)
+{
+    int n = arr.size();
+    int h = 0;
+    vector<int> rank(n), lcp(n);
+
+    for (int i = 0; i < suffixArray.size(); ++i)
+    {
+        rank[suffixArray[i]] = i;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (rank[i] > 0)
+        {
+            int j = suffixArray[rank[i] - 1];
+            while (i + h < n && j + h < n && arr[i + h] == arr[j + h])
+            {
+                ++h;
+            }
+            lcp[rank[i]] = h;
+            if (h > 0)
+            {
+                h -= 1;
+            }
+        }
+    }
+    return lcp;
+}
+int countOccurrences(vector<int> &source, vector<int> &part)
+{
+    if (part.size() > source.size())
+    {
+        return 0;
+    }
+    // compute pi for part
+    vector<int> pi(part.size(), 0);
+    for (int i = 1; i < part.size(); ++i)
+    {
+        int length = pi[i - 1];
+        while (length > 0 && part[i] != part[length])
+        {
+            length = pi[length - 1];
+        }
+        if (part[i] == part[length])
+        {
+            ++length;
+        }
+        pi[i] = length;
+    }
+
+    // use that to match through source
+    int length = 0;
+    int count = 0;
+    for (int i = 1; i < source.size(); ++i)
+    {
+        while (length > 0 && source[i] != part[length])
+        {
+            length = pi[length - 1];
+        }
+        if (source[i] == part[length])
+        {
+            ++length;
+        }
+
+        // check if it was a match
+        if (length == part.size())
+        {
+            length = 0; // reset to prevent overlap matches
+            count++;
+        }
+    }
+    return count;
+}
+pair<int, vector<int>> mostValuableSubarrayV2(vector<int> &tokens, map<int, int> &weights, int k)
+{
+    int n = tokens.size();
+    vector<int> suffixArray = constructSuffixArray(tokens);
+    vector<int> lcpArray = constructLCPArray(tokens, suffixArray);
+
+    int maxWorth = numeric_limits<int>::min();
+    vector<int> best;
+    for (int i = 1; i < n; ++i)
+    {
+        int length = lcpArray[i];
+        if (length == 0)
+        {
+            continue;
+        }
+
+        // calculate weight and collect part
+        int start = suffixArray[i];
+        int weightedSum = 0;
+        vector<int> part;
+        for (int j = 0; j < length; ++j)
+        {
+            weightedSum += weights[tokens[start + j]];
+            part.push_back(tokens[start + j]);
+        }
+        // calculate true worth
+        int counts = countOccurrences(tokens, part);
+        int worth = (counts - 1) * (weightedSum - k);
+        if (worth > maxWorth)
+        {
+            maxWorth = worth;
+            best = std::move(part);
+        }
+    }
+    return {maxWorth, best};
 }
 
 // process
@@ -199,8 +401,10 @@ clang::tooling::Replacements MacroFormatter::process()
     reverseDistinctTokens[distinctTokens[curString]] = curString;
 
     // continuously replace the most valuable subarray while it's worth it
-    auto [worth, sequence] = mostValuableSubarray(tokenNumbers, weights, curString.length());
-    while (worth > 10) // 10 because have to define it and add the trailing newline
+    auto [worth, sequence] = mostValuableSubarrayV2(tokenNumbers, weights, curString.length());
+    // TODO - take into account the fact that this is an identifier, so replacing
+    // punctuation will actually result in an extra space
+    while (worth > 11) // 11 because have to define it and add the newlines
     {
         // replace all instances of the returned subarray
         // with a single token that corresponds to curString
@@ -272,7 +476,7 @@ clang::tooling::Replacements MacroFormatter::process()
         weights[distinctTokens[curString]] = curString.length();
         reverseDistinctTokens[distinctTokens[curString]] = curString;
         // and compute the next most valuable subarray
-        pair<int, vector<int>> result = mostValuableSubarray(tokenNumbers, weights, curString.length());
+        pair<int, vector<int>> result = mostValuableSubarrayV2(tokenNumbers, weights, curString.length());
         worth = result.first;
         sequence = result.second;
     }
